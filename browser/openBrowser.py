dbe50502577c6338fb136b83bfe10eb0ac4e930e
archie94 <arkaprava94@gmail.com>
2016-06-16 00:42:38 +0530

    Open a new session in firefox browser

--- browser/openBrowser.py
+++ browser/openBrowser.py
@@ -3,3 +3,6 @@
 import os
 
 os.system("google-chrome --start-maximized http://www.github.com http://www.facebook.com/ http://www.google.com")
+
+# To open firefox do this 
+#os.system("firefox http://www.github.com http://www.facebook.com/ http://www.google.com")
